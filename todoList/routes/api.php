57ba1958dc032bfaa523a4ee75b3ab27a7ce9719
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-08-03 16:08:52 -0600

    BUILT RESTFUL FUNCTIONS IN ITEM CONTROLLER TO INTERACT WITH DATABASE
    Imported class Item object from models/api.php.
    In itemcontroller edited index function to get Item from database in descending order(get request)
    In itemcontrooler edited store function to post an object with a property name with input key value of name to database(post request)
    In item controller edited update function to idit Item from database.  Used Carbon and the now function to update completed_at property in Item object to time of put request(put request)
    in itemcontroller edited destroy function to delete a todo from Item by id number

--- todoList/routes/api.php
+++ todoList/routes/api.php
@@ -17,6 +17,13 @@ use App\Http\Controllers\ItemController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+    }
+);
 
 Route::get('/items',[ItemController::class, 'index']);
+Route::prefix('/item')->group(function () {
+    Route::post('/store', [ItemController::class, 'store']);
+    Route::put('/{id}', [ItemController::class, 'update']);
+    Route::delete('/{id}', [ItemController::class, 'destroy']);
+    }
+);
